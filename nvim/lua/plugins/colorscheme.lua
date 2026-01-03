b1dbfffe04dc4af82bb9bc7831583fd36609d91e
Aleksandr Nikiforov <alex@alexn.ee>
2026-01-03 22:32:11 +0200

    Colorscheme

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+      styles = {
+        sidebars = "dark",
+        floats = "dark",
+      },
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+      vim.cmd.colorscheme("tokyonight-night")
+    end,
+  },
+}
