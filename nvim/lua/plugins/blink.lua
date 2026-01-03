6047f0a062f0ada54c81a6ed004fb3c8580aa464
Aleksandr Nikiforov <alex@alexn.ee>
2026-01-03 17:26:37 +0200

    Disable autocomplete preselection

---
+++ nvim/lua/plugins/blink.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        list = {
+          selection = {
+            preselect = false, -- 🔴 THIS is the key
+          },
+        },
+      },
+    },
+  },
+}
